fc909f56899cc2313e36495f3a4eede3a4c3e373
boomermath <boomermath13@gmail.com>
2024-11-17 23:20:55 -0500

    Woops, chose an auto!

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -98,6 +98,6 @@ public class RobotContainer {
      * @return the command to run in autonomous
      */
     public Command getAutonomousCommand() {
-        return null;
+        return autonomousManager.getChosenAuto();
     }
 }
--- src/main/java/frc/robot/auto/AutonomousManager.java
+++ src/main/java/frc/robot/auto/AutonomousManager.java
@@ -7,6 +7,9 @@ import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
 import com.pathplanner.lib.util.PIDConstants;
 import com.pathplanner.lib.util.ReplanningConfig;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystem.drivetrain.CommandSwerveDrivetrain;
 import frc.robot.subsystem.drivetrain.TunerConstants;
 import frc.robot.subsystem.elevator.ElevatorPosition;
@@ -20,12 +23,31 @@ public class AutonomousManager {
 
     private final IntakeSubsystem intakeSubsystem;
     private final ElevatorSubsystem elevatorSubsystem;
+    private final CommandSwerveDrivetrain drivetrain;
+
+    private final SendableChooser<Command> autoChooser;
+
 
     @Inject
     public AutonomousManager(CommandSwerveDrivetrain drivetrain, IntakeSubsystem intakeSubsystem, ElevatorSubsystem elevatorSubsystem) {
         this.intakeSubsystem = intakeSubsystem;
         this.elevatorSubsystem = elevatorSubsystem;
+        this.drivetrain = drivetrain;
 
+        configureAutoChooser();
+        this.autoChooser = AutoBuilder.buildAutoChooser();
+    }
+
+    private void configureNamedCommands() {
+        NamedCommands.registerCommand("intakeBall", intakeSubsystem.intake());
+        NamedCommands.registerCommand("depositBall", intakeSubsystem.deposit());
+        NamedCommands.registerCommand("elevatorLow", elevatorSubsystem.toPosition(ElevatorPosition.LOW_GOAL));
+        NamedCommands.registerCommand("elevatorMiddle", elevatorSubsystem.toPosition(ElevatorPosition.MIDDLE_GOAL));
+        NamedCommands.registerCommand("elevatorHigh", elevatorSubsystem.toPosition(ElevatorPosition.HIGH_GOAL));
+        NamedCommands.registerCommand("elevatorDown", elevatorSubsystem.toPosition(ElevatorPosition.HOME));
+    }
+
+    public void configureAutoChooser() {
         configureNamedCommands();
 
         AutoBuilder.configureHolonomic(
@@ -43,14 +65,11 @@ public class AutonomousManager {
                 () -> DriverStation.getAlliance().filter(value -> value == DriverStation.Alliance.Red).isPresent(),
                 drivetrain
         );
+
+        SmartDashboard.putData("Autonomous Routine", AutoBuilder.buildAutoChooser());
     }
 
-    public void configureNamedCommands() {
-        NamedCommands.registerCommand("intakeBall", intakeSubsystem.intake());
-        NamedCommands.registerCommand("depositBall", intakeSubsystem.deposit());
-        NamedCommands.registerCommand("elevatorLow", elevatorSubsystem.toPosition(ElevatorPosition.LOW_GOAL));
-        NamedCommands.registerCommand("elevatorMiddle", elevatorSubsystem.toPosition(ElevatorPosition.MIDDLE_GOAL));
-        NamedCommands.registerCommand("elevatorHigh", elevatorSubsystem.toPosition(ElevatorPosition.HIGH_GOAL));
-        NamedCommands.registerCommand("elevatorDown", elevatorSubsystem.toPosition(ElevatorPosition.HOME));
+    public Command getChosenAuto() {
+        return autoChooser.getSelected();
     }
 }
